4002040af18a6032db30aa2f97b51de8c4677d8d
Adonis <bigirimanaadonis@gmail.com>
2025-10-18 19:28:16 +0300

    RRCA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+test("Test passing", () => {
+  return new Promise((resolve, reject) => {
+    resolve(true);
+  });
+});
